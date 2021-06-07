58a5329f885ec3da29d3ce08ede375d4d4300fc8
Omardyab <omarzadyab@gmail.com>
2021-06-07 17:52:50 +0300

    header and footer done

---
+++ src/componenet/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+render(){
+    return(
+        <p>Copyright &copy; 2021 Omar D'yab</p>
+  
+      );
+}
+
+}
+
+export default Footer;
