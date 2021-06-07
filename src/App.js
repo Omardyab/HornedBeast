e15d6e0d17f917096d04393c8869053974f02217
Omardyab <omarzadyab@gmail.com>
2021-06-07 23:45:39 +0300

    done with branch

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import Footer from './componenet/Footer';
+import Header from './componenet/header';
+import Main from './componenet/Main.js';
 
+
+class App extends React.Component{
+render(){
+    return(
+      
+    <div>
+      <Header/> 
+      <Main/>
+      <Footer/>
+
+    </div>
+      )
+}
+
+}
+
+export default App;
