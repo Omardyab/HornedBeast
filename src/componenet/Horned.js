8ad247a448fbf203161f5606afa2b8afbddaa40a
Omardyab <omarzadyab@gmail.com>
2021-06-08 12:53:11 +0300

    almost done

--- src/componenet/Horned.js
+++ src/componenet/Horned.js
@@ -1,14 +1,39 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card';
 
 
 class Horned extends React.Component{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      HornedVote : 0
+    }
+  }
+  HornedPluseone=()=> {
+    this.state({
+      HornedNum: this.state.HornedNum+1
+
+    })
+  }
 render(){
     return(
         <div>
-      <h2 className='title'>Title : {this.props.title}</h2>
+      {/* <h2 className='title'>Title : {this.props.title}</h2>
       <img src={this.props.url} alt={this.props.description} title={this.props.title}></img>
-      <p className='desc'>Description:{this.props.description}</p>
+      <p className='desc'>Description:{this.props.description}</p> */}
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+      
       </div>
       
         );
--- src/componenet/Main.js
+++ src/componenet/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Horned from './Horned';
-import Cardgroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/CardGroup';
 import data from './data.json';
 
 
@@ -11,13 +11,13 @@ render(){
       {/* <Horned title={'UniWhal'} description={'A unicorn and a narwhal nuzzling their horns'} url={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"}/>
       <Horned title={'Rhino Family'} description={'Mother (or father) rhino with two babies'} url={"https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"}/>
       <Horned title={'Unicorn Head'} description={'Someone wearing a creepy unicorn head mask'} url={"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"}/> */}
-      <Cardgroup>
+      <Card>
         {
         data.map((item)=>{
           return (<Horned title ={item.title} description={item.description} url={item.image_url}/>)
         })
         }
-      </Cardgroup>
+      </Card>
 
       </div>
 
