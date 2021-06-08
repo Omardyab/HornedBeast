3cd431a63122271c09fe6bd78e57a5c9e16b4345
Omardyab <omarzadyab@gmail.com>
2021-06-08 12:58:42 +0300

    card added

--- src/componenet/Horned.js
+++ src/componenet/Horned.js
@@ -25,12 +25,14 @@ render(){
     <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>{this.props.title}</Card.Title>(title is add here )
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+    ❤️={this.state.HornedNum}(update the value of the votes)
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Card.Text>
+    {this.props.description}(update the des)
+    </Card.Text>
+   
   </Card.Body>
 </Card>
       
